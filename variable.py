9e243f1910b0e768b40ea1cb6ec62100b8c25770
IrinaI101 <irinailiyushkina@gmail.com>
2024-11-10 22:13:36 +0500

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,8 @@
+task_count = 12
+print(task_count)
+hours_spent = 1.5
+print(hours_spent)
+course_name = "Python"
+print(course_name)
+time_per_task = (hours_spent / task_count)
+print(time_per_task)
